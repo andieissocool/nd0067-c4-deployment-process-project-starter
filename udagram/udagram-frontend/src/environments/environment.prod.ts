d563c49f94c8ceef2b0d804961c2a8b6ee1b370b
Andrea Haider-P <56677245+andieissocool@users.noreply.github.com>
2025-02-02 20:28:36 +0100

    pass linting

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost:
-    "http://udagram-api-dev2.eba-ujhdxu3h.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://udagram-api-dev2.eba-ujhdxu3h.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
